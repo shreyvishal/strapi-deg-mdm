b9ab1d0d47f3608ea141303481c1e0c26926147e
shreyvishal <shreya.vishal@eminds.ai>
2025-05-11 20:04:26 +0530

    Added: Cron Job and

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -7,8 +7,8 @@ module.exports = {
   myJob: {
     task: ({ strapi }) => {
       const createMeterDataset = async (data) => {
-        return;
         try {
+          const meterReadingTimestamp = new Date().toISOString();
           const meters = await strapi.entityService.findMany(
             "api::meter.meter",
             {
@@ -17,61 +17,117 @@ module.exports = {
                   $notNull: true,
                 },
               },
-              populate: ["energyResource"],
+              populate: {
+                energyResource: {
+                  populate: ["appliances"],
+                },
+              },
             }
           );
           strapi.log.info(
-            `Found ${meters.length} meters with energy resources`
+            `Found ${
+              meters.length
+            } meters with energy resources===> ${JSON.stringify(
+              meters,
+              null,
+              2
+            )}`
           );
+
           // Calculate power factor (random between 0.8 and 1.0)
           strapi.log.info("Starting meter dataset calculations...");
-          const powerFactor = 0.8 + Math.random() * 0.2;
-          strapi.log.debug(`Calculated power factor: ${powerFactor}`);
-          // Calculate kVAh from kWh and power factor
-          const consumptionKVAh = data.consumptionKWh / powerFactor;
-          const productionKVAh = data.productionKWh / powerFactor;
-          strapi.log.debug(
-            `Calculated kVAh - Consumption: ${consumptionKVAh}, Production: ${productionKVAh}`
-          );
-          // Calculate reactive power (kVAR)
-          const consumptionKVAR = Math.sqrt(
-            Math.pow(consumptionKVAh, 2) - Math.pow(data.consumptionKWh, 2)
-          );
-          const productionKVAR = Math.sqrt(
-            Math.pow(productionKVAh, 2) - Math.pow(data.productionKWh, 2)
-          );
-          strapi.log.debug(
-            `Calculated kVAR - Consumption: ${consumptionKVAR}, Production: ${productionKVAR}`
-          );
-          // Calculate voltage (random between 230 and 250)
-          const voltage = 230 + Math.random() * 20;
-          strapi.log.debug(`Calculated voltage: ${voltage}V`);
-          // Calculate current
-          const consumptionCurrent =
-            (data.consumptionKWh * 1000) / (voltage * 1 * 60);
-          const productionCurrent =
-            (data.productionKWh * 1000) / (voltage * 1 * 60);
-          strapi.log.debug(
-            `Calculated current - Consumption: ${consumptionCurrent}A, Production: ${productionCurrent}A`
-          );
-          strapi.log.info("Creating meter dataset record...");
-          const response = await strapi.entityService.create(
-            "api::meter-dataset.meter-dataset",
-            {
+
+          const addMeterLogs = meters.map(async (meter) => {
+            const powerFactor = Number((0.8 + Math.random() * 0.2).toFixed(2));
+            strapi.log.debug(
+              `Calculated power factor for meter Code${meter.code}, with Id ${meter.id} is ${powerFactor}`
+            );
+
+            const totalConsumptionKWh =
+              meter.energyResource.appliances
+                .filter(
+                  (appliance) => appliance.name !== "Solar Panel (production)"
+                )
+                .reduce((acc, appliance) => acc + appliance.baseKWh, 0) *
+              meter?.consumptionLoadFactor;
+            const totalProductionKWh = meter.energyResource.appliances
+              .filter(
+                (appliance) => appliance.name === "Solar Panel (production)"
+              )
+              .reduce((acc, appliance) => {
+                return acc + appliance.baseKWh;
+              }, 0);
+            console.log("totalConsumption===>", totalConsumptionKWh);
+            console.log("totalProduction===>", totalProductionKWh);
+
+            // Calculate kVAh from kWh and power factor
+            const consumptionKVAh = totalConsumptionKWh / powerFactor;
+            const productionKVAh = totalProductionKWh / powerFactor;
+            strapi.log.debug(
+              `Calculated kVAh - Consumption: ${consumptionKVAh}, Production: ${productionKVAh}`
+            );
+
+            // Calculate reactive power (kVAR)
+            const consumptionKVAR = Math.sqrt(
+              Math.pow(consumptionKVAh, 2) - Math.pow(totalConsumptionKWh, 2)
+            );
+            const productionKVAR = Math.sqrt(
+              Math.pow(productionKVAh, 2) - Math.pow(totalProductionKWh, 2)
+            );
+            strapi.log.debug(
+              `Calculated kVAR - Consumption: ${consumptionKVAR}, Production: ${productionKVAR}`
+            );
+
+            // Calculate Voltage (random between 230 and 250)
+            const voltage = Number((230 + Math.random() * 20).toFixed(0));
+            strapi.log.debug(`Calculated voltage: ${voltage}V`);
+
+            // Calculate current
+            const consumptionCurrent =
+              (totalConsumptionKWh * 1000) / (voltage * 1 * 60);
+            const productionCurrent =
+              (totalProductionKWh * 1000) / (voltage * 1 * 60);
+            strapi.log.debug(
+              `Calculated current - Consumption: ${consumptionCurrent}A, Production: ${productionCurrent}A`
+            );
+
+            strapi.log.info("Creating meter dataset record...");
+
+            console.log({
               data: {
-                meter: data.meterId,
-                consumptionKWh: data.consumptionKWh,
-                productionKWh: data.productionKWh,
+                meter: meter.id,
+                consumptionKWh: totalConsumptionKWh,
+                productionKWh: totalProductionKWh,
                 consumptionKVAh: consumptionKVAh,
                 avgCurrent: (consumptionCurrent + productionCurrent) / 2,
                 avgVoltage: voltage,
                 reactivePowerKVAR: consumptionKVAR + productionKVAR,
                 powerFactor: powerFactor,
-                timestamp: data.timestamp,
+                timestamp: meterReadingTimestamp,
                 publishedAt: new Date(),
               },
-            }
-          );
+            });
+
+            return await strapi.entityService.create(
+              "api::meter-dataset.meter-dataset",
+              {
+                data: {
+                  meter: meter.id,
+                  consumptionKWh: totalConsumptionKWh,
+                  productionKWh: totalProductionKWh,
+                  consumptionKVAh: consumptionKVAh,
+                  avgCurrent: (consumptionCurrent + productionCurrent) / 2,
+                  avgVoltage: voltage,
+                  reactivePowerKVAR: consumptionKVAR + productionKVAR,
+                  powerFactor: powerFactor,
+                  timestamp: meterReadingTimestamp,
+                  publishedAt: new Date(),
+                },
+              }
+            );
+          });
+          const response = await Promise.all(addMeterLogs);
+
           strapi.log.info("Successfully created meter dataset record");
           return response;
         } catch (error) {
@@ -79,16 +135,11 @@ module.exports = {
           throw error;
         }
       };
-      const data = {
-        meterId: 1,
-        consumptionKWh: 0.05,
-        productionKWh: 0.02,
-        timestamp: new Date(),
-      };
-      createMeterDataset(data);
+
+      createMeterDataset();
     },
     options: {
-      rule: "*/15 * * * * *",
+      rule: "*/10 * * * * *",
     },
   },
 };
